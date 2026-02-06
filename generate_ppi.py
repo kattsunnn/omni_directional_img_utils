b9799546ac8470de5bc419d2edce885b289c2368
naoki <katsudanaoki@gmail.com>
2026-02-06 17:38:13 +0900

    feat: 返り値に回転行列を追加

--- generate_ppi.py
+++ generate_ppi.py
@@ -1,3 +1,4 @@
+import numpy as np
 import img_utils.img_utils as iu
 from e2p import E2P
 
@@ -9,31 +10,19 @@ def generate_ppi_with_gui(src_img, scale=1, fov_w_deg=30, fov_v_deg=30):
     angle_u_deg, angle_v_deg = ppi_generator.uv_to_angle(u, v)
     ppi_generator.generate_map(fov_w_deg, fov_v_deg, angle_u_deg, angle_v_deg, 0)
     ppi = ppi_generator.generate_img(src_img)
-    return ppi
+    r_mat = E2P.angle_to_r_mat(angle_u_deg, angle_v_deg)
+    return ppi, r_mat
 
 def generate_ppi_from_uv(src_img, fov_w_deg, fov_h_deg, u, v):
     e2p = E2P(src_img.shape[1], src_img.shape[0])
     angle_u_deg, angle_v_deg = e2p.uv_to_angle(u, v)
     e2p.generate_map( fov_w_deg, fov_h_deg, angle_u_deg, angle_v_deg, 0, scale=1.0)
     ppi = e2p.generate_img(src_img)
-    return ppi
+    r_mat = E2P.calc_r_xy(angle_u_deg, angle_v_deg)
+    return ppi, r_mat
 
 
-if __name__ == "__main__":
-
-    import sys
-
-# サンプル：generate_ppi_with_gui
-# 実行例：python .\generate_ppi_gui.py .\sample_img\sample.jpg 0.5 60 30  
-    # input_path = sys.argv[1]
-    # scale = float(sys.argv[2])
-    # fov_w_deg = float(sys.argv[3])
-    # fov_h_deg = float(sys.argv[4])
-
-    # src_img = iu.load_imgs(input_path)
-
-    # ppi = generate_ppi_with_gui(src_img, scale, fov_w_deg, fov_h_deg)
-    # iu.show_imgs(ppi)
+# if __name__ == "__main__":
 
 # サンプル：generate_ppi_from_uv
 # 実行例：python .\generate_ppi.py .\sample\sample.jpg 30 30 .\sample\sample.txt 
--- generate_ppi_with_gui.py
+++ generate_ppi_with_gui.py
@@ -1,16 +1,18 @@
-from generate_ppi import generate_ppi_with_gui
+import click
+from pathlib import Path
 import img_utils.img_utils as iu
-import sys
-import cv2
-input_path = sys.argv[1]
-scale = float(sys.argv[2])
-fov_w_deg = float(sys.argv[3])
-fov_h_deg = float(sys.argv[4])
-output_path = sys.argv[5]
+from generate_ppi import generate_ppi_with_gui
 
-src_img = iu.load_imgs(input_path)
+@click.command
+@iu.prepare_io_path
+@click.option('-ws', '--window_scale', required=True, type=float, help='スケール（必須）')
+@click.option('-fwd', '--fov-w_deg', type=float, required=True, help='画角（横）')
+@click.option('-fhd', '--fov-h_deg', type=float, required=True, help='画角（縦）')
+def main(input_path, output_path, window_scale, fov_w_deg, fov_h_deg):
+    src_img = iu.load_imgs(input_path)
+    ppi, r_mat = generate_ppi_with_gui(src_img, window_scale, fov_w_deg, fov_h_deg)
+    iu.show_imgs(ppi)
+    file_name = Path(input_path).stem + "_ppi"
+    iu.save_imgs(ppi, output_path, file_name=file_name)
 
-ppi = generate_ppi_with_gui(src_img, scale, fov_w_deg, fov_h_deg)
-iu.show_imgs(ppi)
-cv2.imwrite(output_path, ppi)
-# 実行例：python .\generate_ppi_with_gui.py ..\sample\sample.jpg 0.5 60 30  
+main()
