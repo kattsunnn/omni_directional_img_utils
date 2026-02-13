aacb3cb3482850a3c5ac280c29b2ff6d1054c149
naoki <katsudanaoki@gmail.com>
2026-02-13 10:08:41 +0900

    refact: 変数名関数名を変更, 注視点の画像座標と角度座標を取得するメソッドを追加, 画像サイズ返すメソッドの廃止

--- ppi.py
+++ ppi.py
@@ -3,7 +3,7 @@ import numpy as np
 from e2p import E2P
 
 class PPI:
-    def __init__(self, img_e, img_p, angle_u, angle_v):
+    def __init__(self, img_e, img_p, theta_e_deg, phi_e_deg):
         self.img_e = img_e
         self.img_e_h = img_e.shape[0]
         self.img_e_w = img_e.shape[1]
@@ -12,34 +12,36 @@ class PPI:
         self.img_p_h = img_p.shape[0]
         self.img_p_w = img_p.shape[1]
 
-        self.angle_u = angle_u
-        self.angle_v = angle_v
+        self.theta_e_deg = theta_e_deg 
+        self.phi_e_deg = phi_e_deg
 
-    def get_ppi(self):
-        return self.img_p
     def get_src_img(self):
         return self.img_e
-    def get_ppi_h(self):
-        return self.img_p_h
-    def get_ppi_w(self):
-        return self.img_p_w
-    def get_angle_u(self):
-        return self.angle_u
-    def get_angle_v(self):
-        return self.angle_v
+    def get_ppi(self):
+        return self.img_p
+        
     def get_focal_length(self):
         return self.img_e_w / (2*np.pi)
 
-    def get_angular_coordinate(self, u_p, v_p):
+    def get_gaze_point_of_angle_coor(self):
+        return self.theta_e_deg, self.phi_e_deg
+    def get_gaze_point_of_img_coor(self):
+        theta_e_rad = np.deg2rad(self.theta_e_deg)
+        phi_e_rad = np.deg2rad(self.phi_e_deg)
+        u = (theta_e_rad + np.pi) * (self.img_e_w/(2*np.pi))
+        v = (phi_e_rad + (np.pi/2)) * (self.img_e_h/np.pi)
+        return u, v
+
+    def convert_ppi_point_to_angle_coor(self, u_p, v_p):
         # 透視投影画像の画像座標から回転前の3次元視線ベクトルを計算
         focal_length = self.img_e_w/(2*np.pi)
         x = u_p-(self.img_p_w/2)
         y = v_p-(self.img_p_h/2)
         z = focal_length
         # 透視投影画像の生成に使用した回転行列を計算
-        angle_u_rad = np.deg2rad(self.angle_u)
-        angle_v_rad = np.deg2rad(self.angle_v)
-        R = np.dot(E2P.rotation_y(angle_u_rad), E2P.rotation_x(angle_v_rad))
+        theta_e_rad = np.deg2rad(self.theta_e_deg)
+        phi_e_rad = np.deg2rad(self.phi_e_deg)
+        R = np.dot(E2P.rotation_y(theta_e_rad), E2P.rotation_x(phi_e_rad))
         # 回転後の視線ベクトルを計算
         rotated_x = R[0][0] * x + R[0][1] * y + R[0][2] * z
         rotated_y = R[1][0] * x + R[1][1] * y + R[1][2] * z
@@ -47,7 +49,7 @@ class PPI:
         # 回転した視線ベクトルから全方位画像の角度座標を計算
         theta_e, phi_e = E2P.eye_vec_to_angle([rotated_x, rotated_y, rotated_z])
         return theta_e, phi_e
-    
+
     
 
     
