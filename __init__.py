b2aca1a0d2bc2121ce9b2edf39e93d1acff6c6ac
naoki <katsudanaoki@gmail.com>
2026-02-28 13:47:14 +0900

    chore:ライブラリ化

--- __init__.py
+++
@@ -1,2 +0,0 @@
-from .src.e2p import E2P
-from .src.ppi import PPI
--- src/omni_directional_img_utils/e2p.py
+++ src/omni_directional_img_utils/e2p.py
@@ -55,7 +55,7 @@ class E2P:
         return angle_u_deg, angle_v_deg
     
     @staticmethod
-    def eye_vec_to_angle(eye_vec):
+    def gaze_vec_to_angle(eye_vec):
         angle_u_rad = np.arctan2(eye_vec[0], eye_vec[2])
         angle_v_rad = np.arctan2(eye_vec[1], np.sqrt(eye_vec[0]**2 + eye_vec[2]**2))
         angle_u_deg = np.rad2deg(angle_u_rad)
@@ -175,46 +175,3 @@ class E2P:
         if self.map_u is None or self.map_v is None:
             raise Exception("Error: Map is not generated yet. Please call generate_map() first.")
         return cv2.remap(src_img, self.map_u, self.map_v, cv2.INTER_LINEAR)
-
-#Todo: Outputの整理
-if __name__ == '__main__':
-
-
-    import sys
-    from ppi import PPI
-
-    src_img = cv2.imread(sys.argv[1]) # 全方位画像
-    fov_w_deg = float(sys.argv[2])
-    fov_h_deg = float(sys.argv[3])
-    angle_u_deg = float(sys.argv[4])
-    angle_v_deg = float(sys.argv[5])
-    angle_z_deg = float(sys.argv[6])
-
-    e2p = E2P(src_img.shape[1], src_img.shape[0])
-    e2p.generate_map( fov_w_deg, fov_h_deg, 
-                      angle_u_deg, angle_v_deg, angle_z_deg,
-                      scale=1.0)
-    dst_img = e2p.generate_img(src_img)
-
-    ppi = PPI(src_img, dst_img, angle_u_deg, angle_v_deg)
-    print(ppi.get_gaze_point_of_angle_coor())
-    print(ppi.get_gaze_point_of_img_coor())
-    print(ppi.convert_ppi_point_to_angle_coor(ppi.get_ppi().shape[1]/2, ppi.get_ppi().shape[0]/2))
-    cv2.imshow("dst", dst_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    # from ppi import PPI
-
-    # ppi = PPI(src_img, dst_img, angle_u_deg, angle_v_deg)
-    # print(ppi.get_angular_coordinate(dst_img.shape[1]/2, dst_img.shape[0]/2))
-    # # 画像表示
-
-    # e2p = E2P(src_img.shape[1], src_img.shape[0])
-    # e2p.generate_map( fov_w_deg, fov_h_deg, 
-    #                   angle_u_deg, angle_v_deg, angle_z_deg,
-    #                   scale=1.0)
-    # dst_img = e2p.generate_img(src_img)
-
-    # output_path = sys.argv[7]
-    # cv2.imwrite(output_path, dst_img)
